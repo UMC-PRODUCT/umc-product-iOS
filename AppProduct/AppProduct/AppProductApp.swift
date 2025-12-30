babe3937486b4bcac4c73bce2b1c4bab48773a3d
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2025-12-30 13:39:59 +0900

    init: 프로젝트 초기 설정

---
+++ AppProduct/AppProduct/AppProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppProductApp.swift
+//  AppProduct
+//
+//  Created by jaewon Lee on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AppProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
