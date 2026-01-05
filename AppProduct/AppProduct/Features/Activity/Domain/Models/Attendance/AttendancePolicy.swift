17369191f60046b33770688948572b6b473cca62
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2026-01-06 00:01:16 +0900

    feat: 챌린저 활동 관련 도메인 정의

---
+++ AppProduct/AppProduct/Features/Activity/Domain/Models/Attendance/AttendancePolicy.swift
@@ -0,0 +1,13 @@
+//
+//  AttendencePolicy.swift
+//  AppProduct
+//
+//  Created by jaewon Lee on 1/5/26.
+//
+
+import Foundation
+
+enum AttendancePolicy {
+    static let geofenceRadius: Double = 50.0
+    static let lateThresholdMinutes: Int = 10
+}
---
+++ AppProduct/AppProduct/Features/Activity/Domain/Models/Attendance/AttendanceType.swift
@@ -0,0 +1,13 @@
+//
+//  AttendenceType.swift
+//  AppProduct
+//
+//  Created by jaewon Lee on 1/5/26.
+//
+
+import Foundation
+
+enum AttendenceType: String {
+    case gps
+    case reason
+}
