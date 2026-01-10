487b8ec22fac87a113f1f161f4949382e9028145
leeyeji <yeji1135@gachon.ac.kr>
2026-01-10 13:32:13 +0900

    design: CoreStudyManagementList

--- AppProduct/AppProduct/Features/Activity/Presentation/Components/StudyManagementCard.swift
+++ AppProduct/AppProduct/Features/Activity/Presentation/Components/StudyManagementCard.swift
@@ -115,6 +115,6 @@ struct StudyChevronPresenter: View, Equatable {
 // MARK: - Preview
 
 #Preview(traits: .sizeThatFitsLayout) {
-    StudyManagementCard(studyManagementItem: StudyManagementItem(profile: .profile, name: "이예지", school: "가천대학교", part: "iOS", title: "SwiftUI로 화면 구성하기"))
+    StudyManagementCard(studyManagementItem: StudyManagementItem(profile: .profile, name: "이예지", school: "가천대학교", part: "iOS", title: "SwiftUI로 화면 구성하기", state: .examine))
         
 }
