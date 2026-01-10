7471a9d76062ffb2de7adb72cb6e5c92b3fe1d9e
JEONG <ikejhc159@gmail.com>
2026-01-10 21:25:34 +0900

    ✨ [Feat] Firebase Cloud Messaging 및 AppDelegate 구현 (#49)
    - Firebase 푸시 알림 설정 및 FCM 토큰 관리
    - AppDelegate에서 푸시 알림 권한 요청 및 등록
    - FCM 토큰 변경 감지 및 UserDefaults 저장
    - AppStorageKey enum으로 UserDefaults 키 관리
    - DIContainer 연동 지원

---
+++ AppProduct/AppProduct/Core/Common/Enum/AppStorageKey.swift
@@ -0,0 +1,12 @@
+//
+//  AppStorageKey.swift
+//  AppProduct
+//
+//  Created by euijjang97 on 1/10/26.
+//
+
+import Foundation
+
+enum AppStorageKey {
+    static let userFCMToken: String = "UserFCMToken"
+}
