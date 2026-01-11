4145b53c5b35ea14b75c813519e29006ff736e58
leeyeji <yeji1135@gachon.ac.kr>
2026-01-11 19:26:30 +0900

    refactor: 상수 enum

--- AppProduct/AppProduct/Features/Notice/Presentation/Components/NoticeLinkCard.swift
+++ AppProduct/AppProduct/Features/Notice/Presentation/Components/NoticeLinkCard.swift
@@ -11,11 +11,16 @@ import SwiftUI
 struct NoticeLinkCard: View {
     
     // MARK: - Property
-    
     let noticeLinkItem: NoticeLinkItem
     
-    // MARK: - Body
+    // MARK: - Constants
+    fileprivate enum Constants {
+        static let chevronSize: CGSize = .init(width: 4, height: 8)
+        static let innerPadding: CGFloat = 12
+        static let radius: CGFloat = 14
+    }
     
+    // MARK: - Body
     var body: some View {
         HStack {
             LinkIconPresenter()
@@ -26,39 +31,59 @@ struct NoticeLinkCard: View {
             
             Image(systemName: "chevron.right")
                 .resizable()
-                .frame(width: 4, height: 8)
+                .frame(width: Constants.chevronSize.width, height: Constants.chevronSize.height)
                 .foregroundStyle(Color.border)
         }
-        .padding(12)
+        .padding(Constants.innerPadding)
         .background {
-            RoundedRectangle(cornerRadius: 14)
+            RoundedRectangle(cornerRadius: Constants.radius)
                 .foregroundStyle(Color.primary100)
         }
     }
 }
 
+
 // MARK: - LinkIconPresenter
+/// 링크 아이콘
 struct LinkIconPresenter: View {
+    
+    // MARK: - Constants
+    fileprivate enum Constants {
+        static let linkIconSize: CGSize = .init(width: 20, height: 20)
+        static let iconPadding: CGFloat = 10
+        static let iconCornerRadius: CGFloat = 10
+    }
+    
+    // MARK: - Body
     var body: some View {
         Image(systemName: "link")
             .resizable()
-            .frame(width: 20, height: 20)
+            .frame(width: Constants.linkIconSize.width, height: Constants.linkIconSize.height)
             .foregroundStyle(Color.primary700)
-            .padding(10)
+            .padding(Constants.iconPadding)
             .background {
-                RoundedRectangle(cornerRadius: 10)
+                RoundedRectangle(cornerRadius: Constants.iconCornerRadius)
                     .foregroundStyle(Color.primary200)
             }
     }
 }
 
+
 // MARK: - LinkTextPresenter
+/// 링크 텍스트
 struct LinkTextPresenter: View {
     
+    // MARK: - Property
     let url: String
     
+    // MARK: Constants
+    fileprivate enum Constants {
+        static let vstackSpacing: CGFloat = 3
+    }
+    
+    // MARK: - Body
     var body: some View {
-        VStack(alignment: .leading, spacing: 3) {
+        VStack(alignment: .leading, spacing: Constants.vstackSpacing) {
             Text("관련 링크 바로가기")
                 .font(.app(.subheadline, weight: .bold))
             
@@ -68,6 +93,7 @@ struct LinkTextPresenter: View {
         }
     }
 }
+
 
 // MARK: - Preview
 #Preview(traits: .sizeThatFitsLayout) {
