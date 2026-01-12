89a75d160f2792f3979c2be9a29ef86aa722dda0
alwn8918 <alwn8918@gmail.com>
2026-01-13 07:28:18 +0900

    refactor: CommunityFameItem 상수 선언 및 body 분리

--- AppProduct/AppProduct/Core/Common/UIComponents/CommunityFameItem/CommunityFameItem.swift
+++ AppProduct/AppProduct/Core/Common/UIComponents/CommunityFameItem/CommunityFameItem.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+// MARK: - Constant
+
+private enum Constant {
+    static let mainVSpacing: CGFloat = 24
+    static let mainPadding: EdgeInsets = .init(top: 16, leading: 16, bottom: 16, trailing: 16)
+    static let mainRadius: CGFloat = 20
+    // profile
+    static let profileCircleSize: CGSize = .init(width: 39, height: 39)
+    static let nameHSpacing: CGFloat = 8
+    static let partTagPadding: EdgeInsets = .init(top: 2, leading: 8, bottom: 2, trailing: 8)
+    static let partTagRadius: CGFloat = 8
+    // button
+    static let buttonIconSize: CGFloat = 16
+    static let buttonSize: CGSize = .init(width: 64, height: 32)
+    static let buttonRadius: CGFloat = 8
+    // feedback
+    static let feedbackPadding: EdgeInsets = .init(top: 8, leading: 8, bottom: 8, trailing: 8)
+    static let feedbackRadius: CGFloat = 10
+}
+
 // MARK: - CommunityFameItem
 
 /// 커뮤니티탭 - 명예의전당 리스트
@@ -42,119 +62,82 @@ private struct CommunityFameItemPresenter: View, Equatable {
     }
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 24.5) {
+        VStack(alignment: .leading, spacing: Constant.mainVSpacing) {
             HStack {
-                // 프로필
-                if model.profileImage != nil {
-                    model.profileImage
-                } else {
-                    Text(model.userName.prefix(1))
-                        .font(.system(size: 16))
-                        .frame(width: 39, height: 39)
-                        .background(
-                            Circle()
-                                .fill(.gray.opacity(0.2))
-                        )
-                }
-
-                VStack(alignment: .leading, spacing: 2) {
-                    // 이름 + 파트
-                    HStack(spacing: 8) {
-                        Text(model.userName)
-                            .font(.system(size: 14).bold())
-                            .foregroundStyle(.black)
-                        Text(model.part)
-                            .font(.system(size: 10))
-                            .foregroundStyle(.gray)
-                            .padding(.vertical, 2)
-                            .padding(.horizontal, 8)
-                            .background(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .fill(.clear)
-                                    .strokeBorder(.gray.opacity(0.2))
-                            )
-                    }
-
-                    // 워크북
-                    Text(model.workbookTitle)
-                        .font(.system(size: 12))
-                        .foregroundStyle(.gray)
-                }
+                ProfileSection(model: model)
 
                 Spacer()
 
+                // 보기 버튼
                 Button(action: action) {
                     Image(systemName: "square.and.arrow.up")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 16, height: 16)
+                        .font(.system(size: Constant.buttonIconSize))
                     Text("보기")
-                        .font(.system(size: 12))
+                        .appFont(.caption1)
                 }
                 .foregroundStyle(.black)
-                .frame(width: 62, height: 32)
-                .background(
-                    RoundedRectangle(cornerRadius: 8)
-                        .strokeBorder(.gray)
-                )
+                .frame(width: Constant.buttonSize.width, height: Constant.buttonSize.height)
+                .background(.white, in: RoundedRectangle(cornerRadius: Constant.buttonRadius))
+                .overlay(RoundedRectangle(cornerRadius: Constant.buttonRadius).strokeBorder(.gray))
             }
 
             // 피드백 내용
             Text(model.content)
-                .font(.system(size: 12))
-                .foregroundStyle(.black.opacity(0.7))
+                .appFont(.caption1, color: .grey700)
                 .lineLimit(2)
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(8)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(.gray.opacity(0.1))
-                )
+                .padding(Constant.feedbackPadding)
+                .background(.grey100, in: RoundedRectangle(cornerRadius: Constant.feedbackRadius))
         }
-        .padding(16)
-        .background(
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.white)
-        )
+        .padding(Constant.mainPadding)
+        .background(.white, in: RoundedRectangle(cornerRadius: Constant.mainRadius))
+    }
+}
+
+// 프로필
+private struct ProfileSection: View, Equatable {
+    let model: CommunityFameItemModel
+
+    var body: some View {
+        if model.profileImage != nil {
+            model.profileImage
+        } else {
+            Text(model.userName.prefix(1))
+                .appFont(.callout, color: .black)
+                .frame(width: Constant.profileCircleSize.width, height: Constant.profileCircleSize.height)
+                .background(.grey100, in: Circle())
+        }
+
+        VStack(alignment: .leading) {
+            // 이름 + 파트
+            HStack(spacing: Constant.nameHSpacing) {
+                Text(model.userName)
+                    .appFont(.subheadlineEmphasis, color: .black)
+                Text(model.part)
+                    .appFont(.caption2, color: .gray)
+                    .padding(Constant.partTagPadding)
+                    .background(.white, in: RoundedRectangle(cornerRadius: Constant.partTagRadius))
+                    .overlay(RoundedRectangle(cornerRadius: Constant.partTagRadius).strokeBorder(.gray))
+            }
+
+            // 워크북
+            Text(model.workbookTitle)
+                .appFont(.caption1, color: .gray)
+        }
     }
 }
 
 #Preview {
-    struct CommunityFameItemPreview: View {
-        var body: some View {
-            ZStack {
-                Color.grey100
-
-                VStack {
-                    CommunityFameItem(
-                        model: .init(
-                            profileImage: nil,
-                            userName: "김멋사",
-                            part: "Web",
-                            workbookTitle: "React Todo List 만들기",
-                            content: "컴포넌트 분리가 매우 잘 되어있고, 상태 관리가 깔끔합니다."
-                        ),
-                        action: {
-                            print("1번 리스트")
-                        }
-                    )
-
-                    CommunityFameItem(
-                        model: .init(
-                            profileImage: nil,
-                            userName: "이서버",
-                            part: "Server",
-                            workbookTitle: "REST API 설계 및 구현",
-                            content: "RESTful 원칙을 잘 준수하였으며 예외 처리가 훌륭합니다."
-                        ),
-                        action: {
-                            print("2번 리스트")
-                        }
-                    )
-                }
-                .padding()
-            }
+    CommunityFameItem(
+        model: .init(
+            profileImage: nil,
+            userName: "김멋사",
+            part: "Web",
+            workbookTitle: "React Todo List 만들기",
+            content: "컴포넌트 분리가 매우 잘 되어있고, 상태 관리가 깔끔합니다."
+        ),
+        action: {
+            print("1번 리스트")
         }
-    }
-    return CommunityFameItemPreview()
+    )
 }
