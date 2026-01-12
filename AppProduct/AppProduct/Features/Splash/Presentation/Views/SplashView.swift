f969e14fe7e45b1454829d5e91565afff1929560
JEONG <ikejhc159@gmail.com>
2026-01-12 15:29:25 +0900

    feat: 로그인 화면 소셜 로그인 UI 및 로직 구현
    - LoginView, LoginViewModel 추가
    - SocialType, Logo 컴포넌트 추가 및 구현
    - 로그인 관련 이미지 에셋(Apple, Kakao) 추가
    - SplashView 로직 업데이트 및 프로젝트 설정 변경

--- AppProduct/AppProduct/Features/Splash/Presentation/Views/SplashView.swift
+++ AppProduct/AppProduct/Features/Splash/Presentation/Views/SplashView.swift
@@ -10,14 +10,8 @@ import SwiftUI
 struct SplashView: View {
     // MARK: - Property
     @Environment(\.di) var di
-    @Environment(\.colorScheme) var mode
     @State var viewModel: SplashViewModel
-    
-    private enum Constants {
-        static let logoVspacing: CGFloat = 16
-        static let logoSubtitle: String = "University Makeus Challenge"
-    }
-    
+
     // MARK: - Init
     init() {
         self._viewModel = .init(wrappedValue: .init())
@@ -25,21 +19,7 @@ struct SplashView: View {
     
     // MARK: - Body
     var body: some View {
-        VStack(spacing: Constants.logoVspacing, content: {
-            Image(logoImage)
-            
-            Text(Constants.logoSubtitle)
-                .appFont(.body, color: .grey900)
-                .font(.body)
-        })
-    }
-    
-    private var logoImage: ImageResource {
-        if mode == .dark {
-            return .logoDark
-        } else {
-            return .logoLight
-        }
+        Logo()
     }
 }
 
