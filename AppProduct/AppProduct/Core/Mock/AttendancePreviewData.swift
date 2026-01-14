f0dff39a98cc0a9058ff8784dca6680e2ab6df8d
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2026-01-15 02:12:15 +0900

    feat: Mock Data 제작

---
+++ AppProduct/AppProduct/Core/Mock/AttendancePreviewData.swift
@@ -0,0 +1,38 @@
+//
+//  AttendancePreviewData.swift
+//  AppProduct
+//
+//  Created by jaewon Lee on 1/15/26.
+//
+
+import Foundation
+
+#if DEBUG
+struct AttendancePreviewData {
+    static let container = DIContainer()
+    static let errorHandler = ErrorHandler()
+    static let challengerAttendanceUseCase = ChallengerAttendanceUseCase(repository: MockAttendanceRepository())
+        
+    static let sessionId: SessionID = SessionID(value: "iOS_6")
+    static let userId: UserID = UserID(value: "River_")
+    static let coordinate: Coordinate = .init(latitude: 37.582967, longitude: 127.010527)
+    static let attendance: Attendance = .init(
+        sessionId: sessionId,
+        userId: userId,
+        type: .gps,
+        status: .pending,
+        locationVerification: .init(
+            isVerified: true,
+            coordinate: coordinate,
+            address: .init(
+                fullAddress: "한성대학교", city: "서울시", district: "성북구"),
+            verifiedAt: .now),
+        reason: nil)
+    
+    static let session: Session = .init(
+        sessionId: SessionID(value: "iOS_6"),
+        icon: "", title: "Alamofire 파헤치기",
+        week: 6, startTime: Date.now, endTime: Date.now + 10,
+        location: .init(latitude: 37.582967, longitude: 127.010527))
+}
+#endif
