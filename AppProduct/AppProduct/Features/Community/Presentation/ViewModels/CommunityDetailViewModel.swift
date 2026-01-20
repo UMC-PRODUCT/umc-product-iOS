8ca0c804b0e8ace7901bcbf758a179545c078f16
alwn8918 <alwn8918@gmail.com>
2026-01-20 15:13:14 +0900

    design: 커뮤니티 게시글 기본 뷰 제작

--- AppProduct/AppProduct/Features/Community/Presentation/ViewModels/CommunityDetailViewModel.swift
+++ AppProduct/AppProduct/Features/Community/Presentation/ViewModels/CommunityDetailViewModel.swift
@@ -8,4 +8,6 @@
 import Foundation
 
 @Observable
-class CommunityDetailViewModel {}
+class CommunityDetailViewModel {
+    var item: CommunityItemModel = .init(category: .impromptu, tag: .cheerUp, title: "오늘 강남역 카공하실 분?", content: "오후 2시부터 6시까지 강남역 근처 카페에서 각자 할일 하실 분 구합니다! 현재 2명 있어요.", profileImage: nil, userName: "김멋사", part: "iOS", createdAt: "방금 전", likeCount: 2, commentCount: 1)
+}
