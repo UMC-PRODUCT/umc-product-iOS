27aa18fe0c8eafdf4308a4a102ee0be2167e0206
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2026-02-01 03:09:44 +0900

    design: iOS 커리큘럼 mock 데이터에 locked 상태 추가 및 카드 확장 방지

--- AppProduct/AppProduct/Features/Activity/Presentation/Components/Mission/MissionCardHeader.swift
+++ AppProduct/AppProduct/Features/Activity/Presentation/Components/Mission/MissionCardHeader.swift
@@ -62,6 +62,7 @@ struct MissionCardHeader: View, Equatable {
         }
         .contentShape(Rectangle())
         .onTapGesture {
+            if case .locked = model.status { return }
             onToggle()
         }
     }
