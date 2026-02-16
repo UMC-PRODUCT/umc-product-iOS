a190bfaf4edf016fdc5d8feb3654cb8f6793c55d
alwn8918 <alwn8918@gmail.com>
2026-02-16 18:29:52 +0900

    fix: 커뮤니티 게시글 DTO 수정

--- AppProduct/AppProduct/Features/Community/DTO/CommunityPostResponseDTO.swift
+++ AppProduct/AppProduct/Features/Community/DTO/CommunityPostResponseDTO.swift
@@ -77,7 +77,7 @@ extension PostListItemDTO {
             category: category,
             title: title,
             content: content,
-            profileImage: nil,
+            profileImage: authorProfileImage,
             userName: authorName,
             part: authorPart,
             createdAt: ISO8601DateFormatter().date(from: createdAt) ?? Date(),
